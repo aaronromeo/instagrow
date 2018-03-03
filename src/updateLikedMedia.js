8303f507cf1e0572c1c11fc30e6f87a8c0da7194
Aaron Romeo <aaron.romeo@thescore.com>
2018-03-02 21:21:24 -0500

    Liking media and making it a less spammy
    * The previous PR was a little hasty, and had a missing require
    * Adding in a delay so that liking media is less spammy

--- src/updateLikedMedia.js
+++ src/updateLikedMedia.js
@@ -1,8 +1,26 @@
 const Client = require('instagram-private-api').V1;
 const Promise = require('bluebird');
+const _ = require('lodash');
+const moment = require('moment');
 
 const sessionSingleton = require("./services/sessionSingleton");
 const databaseService = require("./services/database");
+
+const MAX_EXECUTION_TIME = 200000;
+
+const likeMedia = (session, account) => {
+  console.log(`Liking ${account.username}\t(${account.instagramId})\t${account.latestMediaUrl} at ${moment()}`)
+  return [
+    new Client.Like.create(session, account.latestMediaId),
+    databaseService.handler.updateLastInteration(account.instagramId, moment().valueOf())
+  ];
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 
 exports.updateLikedMedia = () => sessionSingleton.getSession
   .then((session) => {
@@ -16,7 +34,10 @@ exports.updateLikedMedia = () => sessionSingleton.getSession
         console.log(`${account.username}\t(${account.instagramId})\t${account.latestMediaUrl}`)
       );
     }
-    return Promise.map(_.slice(accountsToBeLiked, 0, 30), accountToBeInteractedWith => {
-      return new Client.Like.create(session, accountToBeInteractedWith.latestMediaId);
+    let nextRun = 0;
+    console.log();
+    return Promise.mapSeries(_.shuffle(accountsToBeLiked), accountToBeInteractedWith => {
+      nextRun = getRandomInt(2000, 10000);
+      return Promise.delay(nextRun).then(() => likeMedia(session, accountToBeInteractedWith));
     });
   })
