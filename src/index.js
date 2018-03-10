9779c1ecc99ce896bfd8018ba40f685bcfced8dc
Aaron Romeo <aaron.romeo@thescore.com>
2018-03-10 01:04:43 -0500

    Script to update the latest user's interaction activity
    * Takes into accounts the account holders activity
    * Should result in less spammy interactions

--- src/index.js
+++ src/index.js
@@ -17,18 +17,52 @@ commander
   });
 
 commander
-  .command('likeMedia <username>')
-  .alias('l')
-  .description('Create "like" interactions for followed accounts who have posted content in the last 3-7 days')
+  .command('updateInteractionActivity <username>')
+  .alias('uia')
+  .description('Checks the users feed and updates the activity')
+  .action((username) => {
+    const config = require(`../config.${username}.json`);
+    const accountsFollowing = require("./getAccountsFollowing");
+    const databaseService = require("./services/database");
+    const latestActivityOfFollowedAccounts = require("./getLatestActivityOfFollowedAccounts");
+
+    databaseService.handler.createInstance(config);
+    accountsFollowing.getAccountsFollowing(config)
+      .then(() => latestActivityOfFollowedAccounts.getLatestActivityOfFollowedAccounts(config))
+      .finally(() => databaseService.handler.getInstance().close());
+  });
+
+commander
+  .command('updateFollowersMedia <username>')
+  .alias('ufm')
+  .description('Update the cached media data of followed accounts who have have not been interacted with in the last 3 days')
   .action((username) => {
     const config = require(`../config.${username}.json`);
     const databaseService = require("./services/database");
     const accountsFollowing = require("./getAccountsFollowing");
     const latestMediaOfFollowedAccounts = require("./getLatestMediaOfFollowedAccounts");
+
+    databaseService.handler.createInstance(config);
+    accountsFollowing.getAccountsFollowing(config)
+      .then(() => latestMediaOfFollowedAccounts.getLatestMediaOfFollowedAccounts(config))
+      .finally(() => databaseService.handler.getInstance().close());
+  });
+
+commander
+  .command('likeMedia <username>')
+  .alias('l')
+  .description('Create "like" interactions for followed accounts based in the cached media data (from updateFollowersMedia) newer than 7 days old')
+  .action((username) => {
+    const config = require(`../config.${username}.json`);
+    const databaseService = require("./services/database");
+    const accountsFollowing = require("./getAccountsFollowing");
+    const latestActivityOfFollowedAccounts = require("./getLatestActivityOfFollowedAccounts");
+    const latestMediaOfFollowedAccounts = require("./getLatestMediaOfFollowedAccounts");
     const likedMedia = require("./updateLikedMedia");
 
     databaseService.handler.createInstance(config);
     accountsFollowing.getAccountsFollowing(config)
+      .then(() => latestActivityOfFollowedAccounts.getLatestActivityOfFollowedAccounts(config))
       .then(() => latestMediaOfFollowedAccounts.getLatestMediaOfFollowedAccounts(config))
       .then(() => likedMedia.updateLikedMedia(config))
       .finally(() => databaseService.handler.getInstance().close());
