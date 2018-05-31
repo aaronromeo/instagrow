4e52bfdd186022a57f99a1ada8a2f6957e9f1f64
Aaron Romeo <aaron.romeo@gmail.com>
2018-05-30 21:23:17 -0400

    Removing some unnecessary vars. (#21)

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ const dynamodbService = require("./services/dynamodb");
 const FOLLOWING_INTERACTION_DELTA_IN_DAYS = 3;
 const FOLLOWER_INTERACTION_DELTA_IN_DAYS = 0;
 const DATABASE_OBJECT = dynamodbService;
+const REGION = "ca-central-1";
 
 exports.settings = {
   DATABASE_OBJECT,
